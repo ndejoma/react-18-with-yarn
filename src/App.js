564826b00a4238836bd60881e975fbff7d902ce2
404Xavier <wakirithomaitho100@protonmai.com>
2022-08-09 09:41:48 +0300

    Add immer to the project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+/** @format */
+
+import React from 'react';
+
+const App = () => {
+	return (
+		<div>
+			<button>Click Me</button>
+		</div>
+	);
+};
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,13 @@
+/** @format */
+
+import React, { StrictMode } from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.querySelector('#root'));
+
+root.render(
+	<StrictMode>
+		<App />
+	</StrictMode>
+);
