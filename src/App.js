0b48ad60e24fdebfea1d3360e6e540fe98b31d81
404Xavier <wakirithomaitho100@protonmai.com>
2022-08-09 11:19:30 +0300

    React challenges done, mutatio of state updates

--- src/App.js
+++ src/App.js
@@ -1,13 +1,167 @@
 /** @format */
 
-import React from 'react';
+import React, { useState } from 'react';
+import { useImmer } from 'use-immer';
+import Canvas from './Canvas';
 
+// const App = () => {
+// 	const [person, setPerson] = useImmer({
+// 		name: 'John Maina',
+// 		age: 18
+// 	});
+// 	return (
+// 		<div>
+// 			<h1>
+// 				Hello {person.name} your age now is {person.age} happy birthday
+// 			</h1>
+// 			<br />
+
+// 			<input
+// 				type='text'
+// 				onChange={e => {
+// 					setPerson(draft => {
+// 						draft.name = e.target.value;
+// 					});
+// 				}}
+// 				name='name'
+// 				value={person.name}
+// 			/>
+// 			<br />
+// 			<br />
+// 			<button
+// 				onClick={e => {
+// 					setPerson(draft => {
+// 						console.dir(draft);
+// 						draft.age++;
+// 					});
+// 				}}>
+// 				Get birthdays
+// 			</button>
+// 		</div>
+// 	);
+// };
+
+// const App = () => {
+// 	const [person, updatePerson] = useImmer({
+// 		name: 'Niki de Saint Phalle',
+// 		artwork: {
+// 			title: 'Blue Nana',
+// 			city: 'Hamburg',
+// 			image: 'https://i.imgur.com/Sd1AgUOm.jpg'
+// 		}
+// 	});
+
+// 	const handleChange = e => {
+// 		const name = e.target.name;
+// 		const value = e.target.value;
+// 		updatePerson(draft => {
+// 			if (name === 'name') {
+// 				draft[name] = value;
+// 			} else {
+// 				draft.artwork[name] = value;
+// 			}
+// 		});
+// 	};
+// 	return (
+// 		<>
+// 			<label>
+// 				Name:
+// 				<input
+// 					name='name'
+// 					value={person.name}
+// 					onChange={handleChange}
+// 				/>
+// 			</label>
+// 			<label>
+// 				Title:
+// 				<input
+// 					name='title'
+// 					value={person.artwork.title}
+// 					onChange={handleChange}
+// 				/>
+// 			</label>
+// 			<label>
+// 				City:
+// 				<input
+// 					name='city'
+// 					value={person.artwork.city}
+// 					onChange={handleChange}
+// 				/>
+// 			</label>
+// 			<label>
+// 				Image:
+// 				<input
+// 					name='image'
+// 					value={person.artwork.image}
+// 					onChange={handleChange}
+// 				/>
+// 			</label>
+// 			<p>
+// 				<i>{person.artwork.title}</i>
+// 				{' by '}
+// 				{person.name}
+// 				<br />
+// 				(located in {person.artwork.city})
+// 			</p>
+// 			<img src={person.artwork.image} alt={person.artwork.title} />
+// 		</>
+// 	);
+// };
+
+// const App = () => {
+// 	const [player, setPlayer] = useImmer({
+// 		firstName: 'Ranjani',
+// 		lastName: 'Shettar',
+// 		score: 10
+// 	});
+
+// 	function handlePlusClick() {
+// 		setPlayer(draft => {
+// 			draft.score++;
+// 		});
+// 	}
+
+// 	function handleFirstNameChange(e) {
+// 		setPlayer(draft => {
+// 			draft.firstName = e.target.value;
+// 		});
+// 	}
+
+// 	function handleLastNameChange(e) {
+// 		setPlayer(draft => {
+// 			draft.lastName = e.target.value;
+// 		});
+// 	}
+
+// 	return (
+// 		<>
+// 			<label>
+// 				Score: <b>{player.score}</b>{' '}
+// 				<button onClick={handlePlusClick}>+1</button>
+// 			</label>
+// 			<label>
+// 				First name:
+// 				<input
+// 					value={player.firstName}
+// 					onChange={handleFirstNameChange}
+// 				/>
+// 			</label>
+// 			<label>
+// 				Last name:
+// 				<input
+// 					value={player.lastName}
+// 					onChange={handleLastNameChange}
+// 				/>
+// 			</label>
+// 		</>
+// 	);
+// };
+
+/***Challenge 2 */
 const App = () => {
 	return (
-		<div>
-			<button>Click Me</button>
-		</div>
-	);
+		<Canvas />
+	)
 };
 
 export default App;
---
+++ src/Canvas.js
@@ -0,0 +1,49 @@
+/** @format */
+import { useState } from 'react';
+import Background from './Background.js';
+import Box from './Box.js';
+import { useImmer } from 'use-immer';
+
+const initialPosition = {
+	x: 0,
+	y: 0
+};
+
+export default function Canvas() {
+	const [shape, setShape] = useImmer({
+		color: 'orange',
+		position: initialPosition
+	});
+
+	function handleMove(dx, dy) {
+		setShape(draft => {
+			draft.position = {
+				x: shape.position.x + dx,
+				y: shape.position.y + dy
+			};
+		});
+	}
+
+	function handleColorChange(e) {
+		setShape(draft => {
+			draft.color = e.target.value;
+		});
+	}
+
+	return (
+		<>
+			<select value={shape.color} onChange={handleColorChange}>
+				<option value='orange'>orange</option>
+				<option value='lightpink'>lightpink</option>
+				<option value='aliceblue'>aliceblue</option>
+			</select>
+			<Background position={initialPosition} />
+			<Box
+				color={shape.color}
+				position={shape.position}
+				onMove={handleMove}>
+				Drag me!
+			</Box>
+		</>
+	);
+}
